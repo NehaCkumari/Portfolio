af848912c91a824292753a94817f568b7faab6f3
Pankaj Kumar <chandravanshi.pankaj@gmail.com>
2020-12-11 21:17:43 +0530

    3rd commit

--- personalweb/settings.py
+++ personalweb/settings.py
@@ -143,3 +143,12 @@ STATICFILES_DIRS =[
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
+
+# email settings
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_HOST_USER='chandravanshi.neha01@gmail.com'
+EMAIL_HOST_PASSWORD='kumarichandra123@'
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render,redirect
+'''from django.shortcuts import render,redirect
 from .models import contact
 from .forms import ContactForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
+from django.core.mail import send_email
 
 
 # Create your views here.
@@ -15,7 +16,33 @@ def conn(request):
         form.save()
         form = ContactForm()
     context = {'form':form}
-    return render(request,'result.html', context)
+    send_mail(form,['chandravanshi.neha01@gmail.com'])
+    return render(request,'result.html', context)'''
+
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from portfolio.forms import ContactForm
+
+def email(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['chandravanshi.neha01@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('thanks')
+    return render(request, "index.html", {'form': form})
+
+def thanks(request):
+    return HttpResponse('Thank you for your message.')
 
 
 
